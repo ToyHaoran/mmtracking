fb9d047e0fd7d0bf961cb8e27cadafd8e89484c2
李浩然 <lihaoran2048@qq.com>
2023-05-08 09:38:42 +0800

    精简代码，删除了与VID无关的代码

--- mmtrack/models/sot/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .prdimp import PrDiMP
-from .siamrpn import SiamRPN
-from .stark import Stark
-
-__all__ = ['SiamRPN', 'Stark', 'PrDiMP']
--- mmtrack/models/vis/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .mask2former import Mask2Former
-from .masktrack_rcnn import MaskTrackRCNN
-
-__all__ = ['MaskTrackRCNN', 'Mask2Former']
